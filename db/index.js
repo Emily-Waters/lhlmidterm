a84a0ac9d57f545510d128e9b11ff4e795cefa71
Emily-Waters <emilyabigailwaters@gmail.com>
2022-01-07 20:23:36 +0000

    changed query in db/index from anonymous function to named function for export

--- db/index.js
+++ db/index.js
@@ -7,11 +7,13 @@ pool.connect(() => {
   console.log('pool connected');
 });
 
+const query = (queryString, queryParams) => {
+  console.log('Query String =================>\n');
+  console.log('Query Params =================>\n');
+  return pool
+    .query(queryString, queryParams);
+};
+
 module.exports = {
-  query: (queryString, queryParams) => {
-    console.log('Query String =================>\n');
-    console.log('Query Params =================>\n');
-    return pool
-      .query(queryString, queryParams);
-  }
+  query
 };
