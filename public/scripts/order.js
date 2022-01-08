da358cca6d7acb3c2e4cca9c6a2464cca6031eba
Emily-Waters <emilyabigailwaters@gmail.com>
2022-01-08 19:37:45 +0000

    created order.js, minor format changes to menu.js

---
+++ public/scripts/order.js
@@ -0,0 +1,62 @@
+$('div.menu-items-container').submit(e => {
+  e.preventDefault();
+  const menuItem = 'id of menu item';     //  TODO:
+  const quantity = 'quantity from form';  //  TODO:
+  addMenuItemToOrder()
+    .then(() => {
+      $('order-container').trigger('reset');//TODO:
+    }).then(() => {
+      loadOrder();
+    })
+    .catch(err => console.log(err.message));
+});
+
+const addMenuItemToOrder = (menuItem, quantity) => {
+  return $.post(`/api/order/${menuItem.id}=${quantity}`);
+};
+
+const createOrderItem = (orderItemData) => {//TODO:
+  return `
+  <div class='order-item'>
+  NUMBER IN LIST
+  DISH NAME
+  QUANTITY
+  PRICE
+  </div>
+  `;
+
+};
+
+const createOrderTotal = () => {
+  // GET order total from db
+  $.get('api/order/total')
+    .then((orderTotal) => {
+
+      return `
+      <div>
+      TOTAL
+      ${orderTotal}
+
+      </div>
+      `;
+    });
+};
+
+const loadOrder = () => {
+  $.get('api/order/:id')
+    .then((orderData) => {
+      renderOrder(orderData);
+    })
+    .catch(err => console.log(err.message));
+
+};
+
+const renderOrder = (orderItems) => {
+  const $orderContainer = $('.order-container');
+  for (const orderItem of orderItems) {
+    let $orderItem = createOrderItem(orderItem);
+    $orderContainer.append($orderItem);
+  }
+  const $orderTotal = createOrderTotal();
+  $orderContainer.append($orderTotal);
+};
