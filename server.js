3cc48133d55759667f7b60d05f50104f6eacab3b
harisrizwan1 <harisrizwan985@gmail.com>
2022-01-07 23:48:46 -0600

    refactor menuQueries and menu.js

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
-// PG database client/connection setup
-const db = require('./db/index');
-
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
@@ -37,8 +34,8 @@ const menuRoutes = require("./routes/menu");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/menu", menuRoutes(db));
+app.use("/api/users", usersRoutes);
+app.use("/api/menu", menuRoutes);
 
 // Note: mount other resources here, using the same pattern above
 
