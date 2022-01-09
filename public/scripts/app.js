aae44f154ab8e9a17aea5b2adc93bcb3d5107260
Julia Gatina <julia.sarandi@gmail.com>
2022-01-09 02:29:31 -0800

    Add order button to load menu on click

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,6 +4,7 @@ const $main = $('main');
 const $menuItemsContainer = $('.menu-items-container');
 const $restaurantsContainer = $('.restaurants-container');
 const $filterOptions = $('#filter-options');
+
 $(document).ready(() => {
   loadRestaurants();
 });
--- public/scripts/restaurants.js
+++ public/scripts/restaurants.js
@@ -17,10 +17,15 @@ const renderRestaurants = (restaurants) => {
   }
 };
 
+// TODO: load menu by restaurant id?
+const loadMenuById = (restaurantId) => {
+  loadMenu();
+  // TODO: create button to show back all restaurants
+  $restaurantsContainer.addClass("d-none");
+};
+
 const createRestaurant = (restaurantItem) => {
-
   return `
-
   <div class="row restaurant-card mb-3">
 
     <img src="${restaurantItem.cover_image_url}" class="col-12 restaurant-img p-0"></img>
@@ -28,10 +33,15 @@ const createRestaurant = (restaurantItem) => {
       ${restaurantItem.name}
     </div>
     <div class="col-12">
-      ${restaurantItem.address}
-    </div>
-    <div class="col-12">
-      ${restaurantItem.phone}
+      <div class="row">
+        <div class="col-6">
+          <p class="mb-0">Address: ${restaurantItem.address}</p>
+          <p>Phone: ${restaurantItem.phone}</p>
+        </div>
+        <div class="col-6 text-right">
+          <button onclick="loadMenuById(${restaurantItem.id})">Order</button>
+        </div>
+      </div>
     </div>
 
   </div>
