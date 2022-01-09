f8ecb8e2be58a1d71a95204c99fc5ae7b488feea
Julia Gatina <julia.sarandi@gmail.com>
2022-01-09 00:51:16 -0800

    Make get All restaurants api

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -2,12 +2,25 @@ const express = require('express');
 const router  = express.Router();
 const restaurantQueries = require('../db/restaurant-queries');
 
-// GET user by id
+// GET restaurant by id
 router.get('/:id', (req, res) => {
   const queryParams = req.params.id;
   restaurantQueries.getRestaurantById(queryParams)
     .then((restaurant) => {
       res.json({ restaurant });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+});
+
+// GET all restaurants
+router.get('/', (req, res) => {
+  restaurantQueries.getAllRestaurants()
+    .then((restaurants) => {
+      res.json(restaurants || []);
     })
     .catch(err => {
       res
