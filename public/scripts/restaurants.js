915ad04ef8dd6da2051dbed3ec42aed69bebaace
Julia Gatina <julia.sarandi@gmail.com>
2022-01-10 13:46:48 -0800

    Remove Order button, make all restaurant card clickable. Add card bootstrap classes.

--- public/scripts/restaurants.js
+++ public/scripts/restaurants.js
@@ -26,24 +26,24 @@ const loadMenuById = (restaurantId) => {
 
 const createRestaurant = (restaurantItem) => {
   return `
-  <div class="row restaurant-card mb-3">
 
-    <img src="${restaurantItem.cover_image_url}" class="col-12 restaurant-img p-0"></img>
-    <div class="col-12 restaurant-title p-2 font-weight-bold text-center">
-      ${restaurantItem.name}
-    </div>
-    <div class="col-12">
-      <div class="row">
-        <div class="col-6">
-          <p class="mb-0">Address: ${restaurantItem.address}</p>
-          <p>Phone: ${restaurantItem.phone}</p>
-        </div>
-        <div class="col-6 text-right">
-          <button onclick="loadMenuById(${restaurantItem.id})">Order</button>
-        </div>
+    <div onclick="loadMenuById(${restaurantItem.id})" class="row restaurant-card card mb-3 pointer">
+
+      <img src="${restaurantItem.cover_image_url}" class="col-12 card-img-top p-0"></img>
+
+      <div class="col-12 card-title p-2 font-weight-bold text-center">
+        ${restaurantItem.name}
       </div>
-    </div>
 
-  </div>
-  `
+        <div class="col-12">
+          <div class="row">
+            <div class="col-6">
+              <p class="mb-0">Address: ${restaurantItem.address}</p>
+              <p>Phone: ${restaurantItem.phone}</p>
+            </div>
+          </div>
+        </div>
+
+    </div>
+    `
 };
