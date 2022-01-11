0c59acb24b950d5ecd63b171233abf64687a983a
Emily-Waters <emilyabigailwaters@gmail.com>
2022-01-11 18:48:24 +0000

    moved listener event handlers into vm-listeners

--- public/scripts/vm-listeners.js
+++ public/scripts/vm-listeners.js
@@ -1,3 +1,15 @@
-const menuCardClick = () => {
+const menuCardSubmit = (e) => {
+  e.preventDefault();
+  const $menuCard = $(e.currentTarget);
+  const menuCardJSON = $menuCard.data().json;
+  console.log('Menu Card Meta Data: ', menuCardJSON);
+  // TODO: MenuCardJSON needs to be sent to the order card
+};
 
+const resCardClick = (e) => {
+  e.preventDefault();
+  const $resCard = $(e.currentTarget);
+  const resCardJSON = $resCard.data().json;
+  console.log('Res Card Meta Data: ', resCardJSON);
+  view.show('menu', resCardJSON);
 };
