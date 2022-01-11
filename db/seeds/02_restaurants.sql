db689d97f9b6122e7928e0e1f9247392b95119d8
Julia Gatina <julia.sarandi@gmail.com>
2022-01-11 09:31:18 -0800

    Fix typo and update menu items name and description

--- db/seeds/02_restaurants.sql
+++ db/seeds/02_restaurants.sql
@@ -1,6 +1,6 @@
 INSERT INTO restaurants (name, phone, address, cover_image_url, thumbnail_image_url)
 VALUES ('Sushi Place', '306 580 9999', '435 1st Ave', 'https://images.unsplash.com/photo-1627462133149-167e7e9a91b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2422&q=80', 'https://image.shutterstock.com/image-photo/delicious-sushi-tokyo-style-600w-648560977.jpg'),
-('The Sashimi Bar', '604 587 4584', '1324 Main Str', 'https://images.unsplash.com/photo-1530260626688-048279320445?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1473&q=80', 'https://image.shutterstock.com/image-photo/fresh-sushi-rolls-on-wooden-600w-1920517436.jpg')
-('Kushi-Ya', '778 776 3425', '510 Burrars Str', 'https://images.unsplash.com/photo-1534482421-64566f976cfa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80', 'https://image.shutterstock.com/image-photo/roll-salmon-tuna-tartare-decorated-600w-1632493312.jpg')
+('The Sashimi Bar', '604 587 4584', '1324 Main St.', 'https://images.unsplash.com/photo-1530260626688-048279320445?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1473&q=80', 'https://image.shutterstock.com/image-photo/fresh-sushi-rolls-on-wooden-600w-1920517436.jpg')
+('Kushi-Ya', '778 776 3425', '510 Burrard St.', 'https://images.unsplash.com/photo-1534482421-64566f976cfa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80', 'https://image.shutterstock.com/image-photo/roll-salmon-tuna-tartare-decorated-600w-1632493312.jpg')
 ;
 
