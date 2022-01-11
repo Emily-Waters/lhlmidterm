2b9f608c87fa22436dea757a7cf733351f33ea6d
Emily-Waters <emilyabigailwaters@gmail.com>
2022-01-11 18:18:11 +0000

    added menu card loading in scripts/view-manager.js

--- public/scripts/view-manager.js
+++ public/scripts/view-manager.js
@@ -5,15 +5,22 @@ $(() => {
   window.view = {};
 
   //  View manager
-  window.view.show = (page = 'home') => {
+  window.view.show = (page = 'home', data) => {
 
     $resContainer.detach();
+    $menuContainer.detach();
 
     switch (page) {
     case 'restaurants':
       $main.append($resContainer);
       break;
     case 'menu':
+      // TODO: need to load menu cards by restaurant id and incorporate filter options
+      getMenuItems()
+        .then(menuData => {
+          menuCards.addManyMenuCards(menuData);
+          $main.append($menuContainer);
+        });
       break;
     case 'order':
       break;
