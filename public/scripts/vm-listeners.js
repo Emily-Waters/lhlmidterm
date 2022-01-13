94e40ab8a99c08b77d5cd55e8d6469057f3bdce2
Julia Gatina <julia.sarandi@gmail.com>
2022-01-13 15:56:10 -0800

    Adjust notifications appearence

--- public/scripts/vm-listeners.js
+++ public/scripts/vm-listeners.js
@@ -64,10 +64,10 @@ $('#is_gluten_free').click(function() {
 const displayNotification = (message) => {
   const alert = `
   <h5><span class="badge badge-secondary" id="notification">${message}</span></h5>`;
-  $('#dropdown-section').prepend(alert);
+  $('#dropdown-section').prepend(alert).fadeIn('slow');
   setTimeout(() => {
-    $('#notification').slideUp();
-  }, 1000);
+    $('#notification').fadeOut('slow').detach();
+  }, 1200);
 
 };
 
@@ -118,8 +118,8 @@ const loginUser = (e) => {
         $('#user-icon-status').toggleClass('icon-active');
         $('#user-sign-card').fadeOut('slow').detach();
         window.cookie = userData;
-        const singedAsBadge = `<h5><span class="badge badge-secondary" id="signed-in">Signed in as ${userData.name}</span></h5>`;
-        $('#dropdown-section').prepend(singedAsBadge);
+        const singedInBadge = `<h5><span class="badge badge-secondary" id="signed-in">Signed in as ${userData.name}</span></h5>`;
+        $('#dropdown-section').prepend(singedInBadge).fadeIn('slow');
       }
       view.show('restaurants');
     })
